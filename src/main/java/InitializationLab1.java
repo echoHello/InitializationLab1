2e3720c03a4e6ef9c3d61d45e516f339610fda5f
Taylor <Taylor@Zipcoder04s-MacBook-Pro.local>
2016-01-25 14:34:59 -0500

    Instantiated rest of ColorfulThing objects

--- src/main/java/InitializationLab1.java
+++ src/main/java/InitializationLab1.java
@@ -27,6 +27,15 @@ class ColorfulThing {
 public class InitializationLab1 {
     public static void main(String[] args) {
         ColorfulThing colorThing = new ColorfulThing("red");
+        ColorfulThing colorThing2 = new ColorfulThing("blue");
+        ColorfulThing colorThing3 = new ColorfulThing("green");
+        ColorfulThing colorThing4 = new ColorfulThing("orange");
+        ColorfulThing colorThing5 = new ColorfulThing("yellow");
         System.out.println(colorThing.getColor());
+        System.out.println(colorThing2.getColor());
+        System.out.println(colorThing3.getColor());
+        System.out.println(colorThing4.getColor());
+        System.out.println(colorThing5.getColor());
+
     }
 }
